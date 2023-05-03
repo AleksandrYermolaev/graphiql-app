fad167d0e05bbd10a51f385b0c7937e3fe2e015a
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-05-03 19:43:01 +0300

    feat: add hoc requireAuth

---
+++ src/hoc/RequireAuth.tsx
@@ -0,0 +1,24 @@
+import { useLocation, Navigate } from 'react-router';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+import { setToken } from 'store/userSlise';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+
+export const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const location = useLocation();
+  const token = localStorage.getItem('token-ff') as string;
+  const dispatch = useAppDispatch();
+
+  if (token) {
+    const decoded = jwtDecode<JwtPayload>(token);
+    const expireTime = decoded.exp;
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (currentTime >= expireTime!) {
+      dispatch(setToken(null));
+      localStorage.removeItem('token-ff');
+      return <Navigate to="/welcome" state={{ from: location }} />;
+    }
+  } else {
+    return <Navigate to="/welcome" state={{ from: location }} />;
+  }
+  return children;
+};
