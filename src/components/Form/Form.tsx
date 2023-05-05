280225f5276175e919ec9610e241a164cad521d9
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-05-05 19:51:43 +0300

    refactor: add orm component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,21 @@
+import { FormattedMessage } from 'react-intl';
+import classes from './Form.module.scss';
+
+type IPropsForm = {
+  title: string;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  children: JSX.Element;
+};
+
+export const Form = ({ title, children, onSubmit }: IPropsForm) => {
+  return (
+    <form className={classes.form} onSubmit={onSubmit}>
+      <fieldset className={classes.fieldset}>
+        <h2 className={classes.title}>
+          <FormattedMessage id={title} />
+        </h2>
+        {children}
+      </fieldset>
+    </form>
+  );
+};
