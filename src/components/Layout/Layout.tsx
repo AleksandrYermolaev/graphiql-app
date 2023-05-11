d790d4659599c8194d0c25f3b25f153aed995fc1
esmolina <elenasmolinann@gmail.com>
2023-05-11 18:00:52 +0300

    refactor: remove navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import classes from './Layout.module.scss';
 import { LOCALES } from '../../IntlLocale/locales';
 import { IntlProvider } from 'react-intl';
@@ -14,12 +14,6 @@ export const Layout: React.FC = () => {
     <IntlProvider messages={messages[locale]} locale={locale} defaultLocale={LOCALES.ENGLISH}>
       <div className={classes.wrapper}>
         <Header />
-        <header className={classes.deleted}>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-          <Link to="/app">Main</Link>
-          <Link to="/">Welcome</Link>
-        </header>
         <main className={classes.main}>
           <Outlet />
         </main>
