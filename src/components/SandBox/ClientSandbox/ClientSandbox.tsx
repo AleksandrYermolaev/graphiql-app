6a133d2d453a744cda446ee8edfc35158a2fd9d1
esmolina <elenasmolinann@gmail.com>
2023-05-16 18:22:09 +0300

    feat: add play-icon

--- src/components/SandBox/ClientSandbox/ClientSandbox.tsx
+++ src/components/SandBox/ClientSandbox/ClientSandbox.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import styles from '../Sandbox.module.scss';
 import { ReactNode } from 'react';
 import { Accordion } from '../../Accordion/Accordion.tsx';
+import { PlayCircle } from 'react-feather';
 
 const cx = classNames.bind(styles);
 
@@ -13,8 +14,10 @@ export const ClientSandbox: React.FC<ClientSandboxProps> = ({ children }) => {
   return (
     <div className={cx('sandbox__client')}>
       <div className={cx('sandbox__section-common', 'sandbox__request')}>
-        <p>request</p>
         {children}
+        <div className={cx('sandbox__reques-icons')}>
+          <PlayCircle color={'#e75b26'} size={'2em'} />
+        </div>
       </div>
       <Accordion />
     </div>
