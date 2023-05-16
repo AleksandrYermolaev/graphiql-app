a87512bb7ba865a70be9ac11156ce0c0ef446add
esmolina <elenasmolinann@gmail.com>
2023-05-16 17:36:33 +0300

    feat: add docs component

--- src/pages/GraphiqlPage/GraphiqlPage.tsx
+++ src/pages/GraphiqlPage/GraphiqlPage.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './GraphiqlPage.module.scss';
 import { Sandbox } from '../../components/SandBox/Sandbox.tsx';
+import { Book, BookOpen } from 'react-feather';
+import { Docs } from '../../components/Docs/Docs.tsx';
+import request from '../../components/SandBox/request.tsx';
 
 const cx = classNames.bind(styles);
 
 export const GraphiqlPage: React.FC = () => {
+  const [showDocs, setShowDocs] = useState(false);
+  function toggleDocsIcon() {
+    setShowDocs(!showDocs);
+  }
+
   return (
     <div className={cx('g__wrapper')}>
+      <div className={cx('g__icons')}>
+        {showDocs && <Book color={'#eee6cc'} onClick={toggleDocsIcon} />}
+        {!showDocs && <BookOpen color={'#eee6cc'} onClick={toggleDocsIcon} />}
+      </div>
+      {!showDocs && <Docs children={request()} />}
       <Sandbox />
     </div>
   );
