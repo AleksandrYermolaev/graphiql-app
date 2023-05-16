23ce1baff93356325409328b5a5736b163a65a76
esmolina <elenasmolinann@gmail.com>
2023-05-16 08:30:53 +0300

    feat: add editor template

--- src/pages/GraphiqlPage/GraphiqlPage.tsx
+++ src/pages/GraphiqlPage/GraphiqlPage.tsx
@@ -1,3 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './GraphiqlPage.module.scss';
+
+const cx = classNames.bind(styles);
+
 export const GraphiqlPage: React.FC = () => {
-  return <p>Main page</p>;
+  return (
+    <div className={cx('g__wrapper')}>
+      <div className={cx('g__sandbox')}>
+        <div className={cx('g__section-common', 'g__request')}>request</div>
+        <div className={cx('g__section-common', 'g__response')}>response</div>
+      </div>
+    </div>
+  );
 };
