4fb4aa7f33e649b3389bb452e12e47c31872f59f
Aleksandr Yermolaev <sanyagt50@gmail.com>
2023-05-17 10:08:20 +0300

    refactor: move editor to SecondaryEditor component

---
+++ src/components/SecondaryEditor/SecondaryEditor.tsx
@@ -0,0 +1,25 @@
+import AceEditor from 'react-ace';
+import ace from 'ace-builds/src-noconflict/ace';
+ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/');
+ace.config.setModuleUrl(
+  'ace/mode/javascript_worker',
+  'https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-javascript.js'
+);
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/worker-json';
+
+export const SecondaryEditor: React.FC = () => {
+  return (
+    <AceEditor
+      placeholder="write here..."
+      mode={'json'}
+      theme="monokai"
+      fontSize={16}
+      showPrintMargin={false}
+      highlightActiveLine={false}
+      tabSize={2}
+      onChange={() => console.log('edit')}
+    />
+  );
+};
--- src/pages/GraphiqlPage/GraphiqlPage.tsx
+++ src/pages/GraphiqlPage/GraphiqlPage.tsx
@@ -1,33 +1,3 @@
-import AceEditor from 'react-ace';
-import ace from 'ace-builds/src-noconflict/ace';
-ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/');
-ace.config.setModuleUrl(
-  'ace/mode/javascript_worker',
-  'https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-javascript.js'
-);
-import 'ace-builds/src-noconflict/mode-json';
-import 'ace-builds/src-noconflict/theme-monokai';
-import 'ace-builds/src-noconflict/worker-json';
-import { useState } from 'react';
-
 export const GraphiqlPage: React.FC = () => {
-  const [, setState] = useState<string>('');
-
-  const handleChange = (newValue: string) => {
-    console.log(newValue);
-    setState(newValue);
-  };
-
-  return (
-    <AceEditor
-      placeholder="write here..."
-      mode={'json'}
-      theme="monokai"
-      fontSize={16}
-      showPrintMargin={false}
-      highlightActiveLine={false}
-      tabSize={2}
-      onChange={handleChange}
-    />
-  );
+  return <p>Main page</p>;
 };
