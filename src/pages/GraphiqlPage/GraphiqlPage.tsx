7ad10cf961c0073d53e0a2b7a59e0c23b5fbf2b8
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-05-23 16:40:03 +0300

    fix: fixed checked token

--- src/pages/GraphiqlPage/GraphiqlPage.tsx
+++ src/pages/GraphiqlPage/GraphiqlPage.tsx
@@ -1,20 +1,45 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import styles from './GraphiqlPage.module.scss';
 import { Transition } from 'react-transition-group';
 import { Sandbox } from '../../components/SandBox/Sandbox.tsx';
 import { Book, BookOpen } from 'react-feather';
 import { Docs } from '../../components/Docs/Docs.tsx';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { setToken } from 'store/userSlice.ts';
+import { useAppDispatch, useAppSelector } from 'hooks/useAppDispatch.ts';
+import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 export const GraphiqlPage: React.FC = () => {
   const nodeRef = useRef(null);
   const [showDocs, setShowDocs] = useState(false);
+  const auth = getAuth();
+  const token = useAppSelector((state) => state.userInfo.token);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   function toggleDocsIcon() {
     setShowDocs(!showDocs);
   }
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/', { replace: true });
+    }
+  }, [navigate, token]);
+
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const accessToken = await user.getIdToken();
+      dispatch(setToken(accessToken));
+      localStorage.setItem('token-ff', accessToken);
+    } else {
+      dispatch(setToken(null));
+      localStorage.removeItem('token-ff');
+    }
+  });
 
   return (
     <div className={cx('g__wrapper')}>
